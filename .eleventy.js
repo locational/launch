e39ae72145878f86ad138d0a6a0d27bc41439daa
Jonathan Smith <jonathan@peoplesized.com>
2020-04-17 16:28:15 +0100

    comment

--- .eleventy.js
+++ .eleventy.js
@@ -1,5 +1,7 @@
 module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy("assets");
+
+  // For old index.html
   eleventyConfig.addPassthroughCopy("css");
   eleventyConfig.addPassthroughCopy("images");
   eleventyConfig.addPassthroughCopy("js");
